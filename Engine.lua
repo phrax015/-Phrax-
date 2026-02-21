0626ab8133766323629bc6619ab8eae79df0491c
phrax015 <vicotuxler25@gmail.com>
2026-02-22 00:05:47 +0100

    Update Engine.lua

--- Engine.lua
+++ Engine.lua
@@ -1,7 +1,7 @@
 local Hydra = {}
 
 function Hydra.MakeNotification(txt, time)
-    local p = loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxAvatar/Hydra/main/Notify.lua"))()
+    local p = loadstring(game:HttpGet("https://raw.githubusercontent.com/phrax015/-Phrax-/refs/heads/main/Notify.lua"))()
 	p:MakeNotification({Name = "Hydra", Content = txt, Time = time})
 end
 
